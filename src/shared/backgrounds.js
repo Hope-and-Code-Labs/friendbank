3ac978e4212182a6ef2a9a00c0a3177afc29da66
Shai Sachs <shaisachs@gmail.com>
2022-06-27 15:49:48 -0400

    delete old cruft
    Signed-off-by: Shai Sachs <shaisachs@gmail.com>

--- src/shared/backgrounds.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // 'ed-aoc-icecream': {
-  //   type: 'image',
-  //   source: 'https://ed-markey-supporter-photos.s3.amazonaws.com/Ed+AOC+Icecream.png',
-  //   alt: 'Ed and Rep. Alexandria Ocasio-Cortez eating ice cream together',
-  // },
-};
